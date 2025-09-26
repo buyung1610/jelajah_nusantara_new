80f43494426ba5fa24d53959455a12dbdda6e8a4
fajar buyung <fajarbuyung65@gmail.com>
2025-09-26 13:29:28 +0700

    detail-artikel

--- lib/screens/articles/detail_screen.dart
+++ lib/screens/articles/detail_screen.dart
@@ -17,7 +17,11 @@ class Detailscreen extends StatelessWidget {
                   children: [
                     ClipRRect(
                       borderRadius: BorderRadius.circular(10),
-                      child: Image.asset("assets/images/nusa-penida.png"),
+                      child: Image.asset(
+                        "assets/images/bromo.png",
+                        width: double.infinity,
+                        fit: BoxFit.cover,
+                      ),
                     ),
                     Positioned(
                       top: 10,
@@ -30,7 +34,7 @@ class Detailscreen extends StatelessWidget {
                           color: Colors.white,
                         ),
                         child: IconButton(
-                          onPressed: () => Navigator.of(context).pop(),
+                          onPressed: () {},
                           icon: const Icon(
                             Icons.arrow_back_ios,
                             color: Color(0XFFD1A824),
@@ -42,13 +46,93 @@ class Detailscreen extends StatelessWidget {
                   ],
                 ),
                 SizedBox(height: 20),
-                Text("Gunung Bromo", style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
+                Text(
+                  "Gunung Bromo", 
+                  style: TextStyle(
+                    fontSize: 24, 
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
                 SizedBox(height: 10),
                 Text(
                   "Indonesia, negara kepulauan yang kaya akan keindahan alamnya, memiliki sejumlah gunung berapi yang menakjubkan, salah satunya adalah Gunung Bromo. Terletak di Taman Nasional Bromo Tengger Semeru, Jawa Timur, Gunung Bromo telah menjadi daya tarik utama bagi wisatawan domestik dan mancanegara. Dengan ketinggian sekitar 2.329 meter di atas permukaan laut, Gunung Bromo adalah salah satu gunung berapi paling aktif di Indonesia. Pemandangan kawah yang luas dengan latar belakang langit biru dan awan putih telah menjadi pemandangan ikonik yang memikat banyak pengunjung. Tidak hanya terbatas pada kawahnya, kawasan sekitarnya juga menawarkan panorama alam yang memukau, termasuk lautan pasir serta dikelilingi oleh pegunungan hijau yang menjulang.\n\n"
                   "Taman Nasional Bromo Tengger Semeru juga dikenal karena keberadaan masyarakat Tengger yang menjalankan tradisi dan ritual unik. Salah satunya adalah upacara Kasada yang dilakukan setiap tahun pada bulan purnama di tengah-tengah kawah Gunung Bromo. Dalam upacara ini, masyarakat Tengger memberikan persembahan kepada Sang Hyang Widhi (Tuhan Yang Maha Esa) sebagai tanda syukur atas panen yang melimpah. Ritual ini menarik perhatian banyak wisatawan yang ingin menyaksikan keunikan budaya lokal.\n\n"
-                  "Selain pendakian, terdapat beragam kegiatan wisata lain yang dapat dinikmati di sekitar Gunung Bromo. Wisatawan dapat menikmati perjalanan menunggang kuda melintasi lautan pasir, mengunjungi Pura Luhur Poten yang berada di kaki Gunung Bromo, atau sekedar menikmati matahari terbit yang memukau dari puncak Penanjakan. Tidak ketinggalan untuk Anda yang gemar berswafoto, Gunung Bromo memiliki berbagai tempat dan pemandangan menakjubkan yang layak untuk diabadikan.\n\n",
+                  "Selain pendakian, terdapat beragam kegiatan wisata lain yang dapat dinikmati di sekitar Gunung Bromo. Wisatawan dapat menikmati perjalanan menunggang kuda melintasi lautan pasir, mengunjungi Pura Luhur Poten yang berada di kaki Gunung Bromo, atau sekedar menikmati matahari terbit yang memukau dari puncak Penanjakan. Tidak ketinggalan untuk Anda yang gemar berswafoto, Gunung Bromo memiliki berbagai tempat dan pemandangan menakjubkan yang layak untuk diabadikan.",
                   textAlign: TextAlign.justify,
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Informasi Artikel',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 15,
+                      backgroundColor: Color(0XFFD1A824),
+                      child: Icon(
+                        Icons.location_on_outlined,
+                        color: Colors.white,
+                        size: 20,
+                      )
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "Gunung Bromo",
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 15,
+                      backgroundColor: Color(0XFFD1A824),
+                      child: Icon(
+                        Icons.person_2_rounded,
+                        color: Colors.white,
+                        size: 20,
+                      )
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "Author Name",
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 15,
+                      backgroundColor: Color(0XFFD1A824),
+                      child: Icon(
+                        Icons.calendar_month_outlined,
+                        color: Colors.white,
+                        size: 20,
+                      )
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "9-26-2025",
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                    )
+                  ],
                 ),
               ],
             ),
